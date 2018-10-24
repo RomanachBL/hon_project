4717bc9b7ea18ed8be9eca9015d5d7696fcf7e15
RomanachBL <carrey.romain@gmail.com>
2018-10-25 00:52:33 +0200

    Commit graphique

--- src/hon_project/Fenetre.java
+++ src/hon_project/Fenetre.java
@@ -1,30 +1,67 @@
 package hon_project;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Fenetre extends JFrame {
-
+	
+	  //On initialise les zones de textes et leurs labels
+	  private JPanel container = new JPanel();
+	  private JLabel label_1 = new JLabel("Pierre : ");
+	  private JTextField jtf_1 = new JTextField();
+	  private JLabel label_2 = new JLabel("Paul : ");
+	  private JTextField jtf_2 = new JTextField();
+	  private JLabel label_3 = new JLabel("Jacques : ");
+	  private JTextField jtf_3 = new JTextField();
+	
     public Fenetre(){
 
-        this.setTitle("Ma première fenêtre Java");
-        this.setSize(400, 200);
+    	// La fenetre
+        this.setTitle("Ma fenêtre Java");
+        this.pack();
+        this.setDefaultLookAndFeelDecorated(true);
+        this.setExtendedState(this.MAXIMIZED_BOTH);
         this.setLocationRelativeTo(null);
-/*
-        //Instanciation d'un objet JPanel
-        JPanel pan = new JPanel();
-        //Définition de sa couleur de fond
-        pan.setBackground(Color.GREEN);
-        //On prévient notre JFrame que notre JPanel sera son contentPane
-        this.setContentPane(pan);
-*/
-        // Le panneau
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setContentPane(new Panneau());
-
+        
+        // La zone de texte
+        JPanel top = new JPanel();
+        Font police = new Font("Arial", Font.BOLD, 14);
+        // Pierre
+        jtf_1.setFont(police);
+        jtf_1.setPreferredSize(new Dimension(150, 30));
+        top.add(label_1);
+        top.add(jtf_1);
+ /*       // Paul
+        jtf_2.setFont(police);
+        jtf_2.setPreferredSize(new Dimension(150, 30));
+        top.add(label_2);
+        top.add(jtf_2);
+        //Jacques
+        jtf_3.setFont(police);
+        jtf_3.setPreferredSize(new Dimension(150, 30));
+        top.add(label_3);
+        top.add(jtf_3);
+*/        
+        // Les add 
+        //container.setLayout(new GridLayout(2,0));
+        container.add(new Panneau());  
+        container.add(top);  
+        
+        // Affichons
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(container);
         this.setVisible(true);
 
     }
 
 }
+          
--- src/hon_project/Panneau.java
+++ src/hon_project/Panneau.java
@@ -1,17 +1,45 @@
 package hon_project;
 
+import java.awt.AlphaComposite;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 public class Panneau extends JPanel {
 
     public void paintComponent(Graphics g){
+    	Font font = new Font("Courier", Font.BOLD, 30);
+        g.setFont(font);
+        g.setColor(Color.DARK_GRAY);          
+        g.drawString("Honoraires :", 20, 40); 
 
-        //Vous verrez cette phrase chaque fois que la méthode sera invoquée
-        System.out.println("Je suis exécutée !");
+        try {
+        	
+            Image img = ImageIO.read(new File ("/home/romain/master_2/java_workspace/hon_project/src/hon_project/2695.png"));
+            
+            //On met l'image au milieu avec Graphics2D
+            Graphics2D g2d = (Graphics2D) g;
+            int x = (this.getWidth() - img.getWidth(null)) / 2;
+            int y = (this.getHeight() - img.getHeight(null)) / 2;
+            
+            //On met l'opacity
+            float opacity = 0.3f;
+            g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, opacity));
+            
+            //Et on l'affiche
+            g2d.drawImage(img, x, y, null);
 
-        g.fillOval(20, 20, 75, 75);
-
+            
+          } catch (IOException e) {
+            e.printStackTrace();
+          }     
     }
 
 }
